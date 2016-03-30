7a3c6f09f67b567944c8aa4888d2257d9d95021e
abe_clark <abeaclark@gmail.com>
2016-03-29 17:13:21 -0700

    modify post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -57,7 +57,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to '/'} # , notice: 'Post was successfully created.'
+        format.html { redirect_to '/?sort=newest'} # , notice: 'Post was successfully created.'
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
